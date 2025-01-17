e1d267c8912b21498bcaca78b4ab770243b2346a
Thirisha12 <106610702+Thirisha12@users.noreply.github.com>
2025-01-17 16:55:00 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,11 +31,23 @@ const HomePage = ({ pokemonList, onPokemonClick, onSearch, onFilter, onSort, onN
       <div className="filters">
         <input type="text" placeholder="Search Pokemon" onChange={(e) => onSearch(e.target.value)} />
         <select onChange={(e) => onFilter(e.target.value)}>
-          <option value="">Filter by Type</option>
+         <option value="">Filter by Type</option>
           <option value="poison">Poison</option>
           <option value="bug">Bug</option>
           <option value="fire">Fire</option>
           <option value="ice">Ice</option>
+          <option value="grass">Grass</option>
+          <option value="electric">Electric</option>
+          <option value="fairy">Fairy</option>
+          <option value="normal">Normal</option>
+          <option value="ghost">Ghost</option>
+          <option value="dragon">Dragon</option>
+          <option value="dark">Dark</option>
+          <option value="rock">Rock</option>
+          <option value="fighting">Fighting</option>
+          <option value="psychic">Psychic</option>
+          <option value="steel">Steel</option>
+          <option value="flying">Flying</option>
         </select>
         <button onClick={() => onSort('asc')}>Sort by Name (Asc)</button>
         <button onClick={() => onSort('desc')}>Sort by Name (Desc)</button>
